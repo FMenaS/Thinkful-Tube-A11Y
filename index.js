ae464613260cfead447854fbd1d95fc4de2b3aaf
Fernando Mena <Fernando.mena.sanchez@gmail.com>
2018-03-19 13:14:51 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const YOUTUBE_SEARCH_URL = 'https://www.googleapis.com/youtube/v3/search';
+
+function getDataFromApi(searchTerm, callback) {
+  const query = {
+    part: 'snippet',
+    key: 'AIzaSyCQ9I8BEmCOsfg342nWOuVakVr7CEQ1XeY',
+    q: `${searchTerm} in:name`,
+  };
+
+  $.getJSON(YOUTUBE_SEARCH_URL, query, callback);
+}
+
+function renderResult(result) {
+  return `
+    <div>
+	    <a href="https://www.youtube.com/watch?v=${result.id.videoId}" target="_blank"><img src="${result.snippet.thumbnails.medium.url}"></a>
+	  </div> 
+  `;
+}
+
+//how exactly does this function work? Callback function?
+function displayYouTubeSearchData(data) {
+  const results = data.items.map((item, index) => renderResult(item));
+  $('.js-search-results').html(results);
+}
+
+function watchSubmit() {
+  $('.js-search-form').submit(event => {
+    event.preventDefault();
+    const queryTarget = $(event.currentTarget).find('.js-query');
+    const query = queryTarget.val();
+    // clear out the input
+    queryTarget.val("");
+    getDataFromApi(query, displayYouTubeSearchData);
+  });
+}
+
+$(watchSubmit);
